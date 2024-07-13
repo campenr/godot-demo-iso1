746e45354d89d081021b0ad68c3bcf8de850f821
DESKTOP-QA4T6G4\richa <richard@campen.co>
2024-07-13 13:47:10 +1200

    Add basic projectile attack, and a simple enemy that the projectile can hit.

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,11 @@ extends CharacterBody2D
 const SPEED = 100
 
 var target: Vector2
+var projectile_scene
+
+
+func _ready():
+	projectile_scene = load("res://scenes/projectile.tscn")
 
 
 func _physics_process(_delta):
@@ -15,11 +20,18 @@ func _physics_process(_delta):
 
 func _input(event):
 	if event is InputEventMouseButton:
-		if event.is_pressed():
+		if event.is_pressed() and event.button_index == 1:
 			move_to_point()
+		if event.is_pressed() and event.button_index == 2:
+			attack_projectile()
 
 
 func move_to_point():
 	target = get_global_mouse_position()
 	velocity = (target - position).normalized() * SPEED
 
+
+func attack_projectile():
+	var projectile = projectile_scene.instantiate()
+	projectile.set_position(position)
+	owner.add_child(projectile)
